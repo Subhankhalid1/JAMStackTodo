88a6fe48f6a882b8f0882384001a1b4d3ab850db
Subhankhalid1 <rsubhakhalid@gmail.com>
2020-11-05 03:03:05 +0500

    Added Theme-Ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import {Container,Button} from 'theme-ui';
+import { Heading, Flex } from "theme-ui";
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default props=> (
+   <Container>
+     <Flex sx={{flexDirection:"column", padding:5}}>
+   <Heading as="h1">Todo App</Heading>
+   <Button sx={{color:"black", marginTop:3}} onClick={()=>{alert("Click me")}} >Login</Button>
+   </Flex>
+    </Container>
+);
